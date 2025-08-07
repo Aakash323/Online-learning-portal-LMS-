af4f6c186e74229eedffc6cad2bde7d3f550c446
Aakash Poudel <160621659+Aakash323@users.noreply.github.com>
2025-08-08 02:33:15 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'online-learning-portal-4fonmkx37-aakash-poudels-projects.vercel.app',
     credentials: true,
 }));
 
